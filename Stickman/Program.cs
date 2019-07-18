7f4bb0de9e092f208e443dd0c85d9b6ffdfddbde
NeuroWhAI <tlsehdgus0212@gmail.com>
2019-07-18 18:36:52 +0900

    Edit the welcome message

--- Stickman/Program.cs
+++ Stickman/Program.cs
@@ -52,7 +52,8 @@ namespace Stickman
                     .WithDescription(@"카페 공식 디스코드 서버 입장을 환영합니다!
 먼저 별명을 카페 닉네임과 동일하게 바꿔주시고
 [여기](https://cafe.naver.com/powdertoy/51728)에서 댓글로 자신을 인증해주세요.
-그 후 기다리시면 스탭이 멤버 권한을 부여해드릴 것입니다.");
+그 후 기다리시면 스탭이 멤버 권한을 부여해드릴 것입니다.
+권한을 부여받으신 후 꼭 공지 채널을 먼저 확인해주세요!");
 
             await e.Member.SendMessageAsync(embed: embed.Build());
         }
